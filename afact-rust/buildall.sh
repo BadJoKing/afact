a3735ff3d8ac5a0e7ccd6992021a838480feb7df
BadJoKing <100881959+BadJoKing@users.noreply.github.com>
2023-10-07 21:49:41 +0200

    Delete afact-rust directory

--- afact-rust/buildall.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install -y gcc-multilib mingw*
-cargo build --target x86_64-pc-windows-gnu
-cargo build --target x86_64-unknown-linux-gnu
-cargo build --target i686-unknown-linux-gnu
-cargo build --target i686-pc-windows-gnu
-read a
